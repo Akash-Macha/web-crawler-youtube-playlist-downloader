c53c44670d1b13ac6bc7629e4d19c71cdca6f502
Akash Macha <akashmacha1999@gmail.com>
2018-12-11 13:17:28 +0530

    added main.py and web_crawler.py

---
+++ main.py
@@ -0,0 +1,102 @@
+from tkinter import *
+from web_crawler import *
+
+
+def sendUrl(event):
+    url = entry.get()
+    extract(url)
+
+
+def clearEntry(event):
+    entry.delete(0, 'end')
+
+
+def motion(event):  # https://stackoverflow.com/a/22925718
+    x, y = event.x, event.y
+    return str(str(x) + ", " + str(y))
+
+
+def doNothing():
+    print("Crap")
+
+
+def create_menu(root):
+    menu = Menu(root)
+    # we should configure the menu
+    root.config(menu=menu)
+
+    subMenu = Menu(menu)
+
+    # drop down functionality is called 'cascading' in tkinter
+    menu.add_cascade(label="File", menu=subMenu)
+
+    # adding items to subMenu and giving functionality to it
+    subMenu.add_command(label="New Project...", command=doNothing)
+    subMenu.add_command(label="New...", command=doNothing)
+
+    # a line to separate the items in the menu
+    subMenu.add_separator()
+
+    subMenu.add_command(label="Exit", command=doNothing)
+
+    # creating an other menu item
+
+    editMenu = Menu(menu)
+    menu.add_cascade(label='Edit', menu=editMenu)
+    editMenu.add_command(label="Redu", command=doNothing)
+
+
+def create_statusBar(root):
+    # statusBar = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
+    currentMousePosition = "Waiting for the url..."
+    statusBar = Label(root, text=currentMousePosition, bd=1, relief=SUNKEN, anchor=W)
+    statusBar.pack(side=BOTTOM, fill=X)
+
+
+def create_window():
+    root = Tk()
+    root.title("Youtube playlist title's extractor")
+    root.geometry("800x200")
+
+    topFrame = Frame(root)
+    topFrame.pack(side=TOP)
+
+    myLabel_1 = Label(topFrame, text="Youtube Play List Title Extractor", fg='black')
+    myLabel_1.config(font=("Helvetica 16 bold italic", 30))
+    myLabel_1.pack()
+
+    middleFrame = Frame(root)
+    middleFrame.pack(side=TOP)
+
+    myLabel_2 = Label(middleFrame, text="Paste URL: ", fg='black')
+    myLabel_2.grid(row=0, sticky=E)
+
+    global entry
+    entry = Entry(middleFrame, text="Paste Url: ", width=80)
+    entry.grid(row=0, column=1)
+    entry.focus_set()
+
+    myButton = Button(middleFrame, text="Extract", height=5, width=20)
+    myButton.bind("<Button-1>", sendUrl)
+    myButton.grid(row=1, column=1)
+
+    clearTextButton = Button(middleFrame, text="Clear")
+    clearTextButton.bind("<Button-1>", clearEntry)
+    clearTextButton.grid(row=1, column=2)
+
+    create_menu(root)
+
+    create_statusBar(root)
+
+    root.mainloop()
+
+def main():
+    print("Youtube PlayList Title Extractor!\nYou've 2 modes:\n1.GUI Mode\n2.CommandLine Mode")
+    choice = int(input("Your option(1 or 2): "))
+    if choice == 1:
+        create_window()
+    else:
+        print("\nI'll make it work later!\nThanks for using!\n")
+    
+if __name__ == '__main__':
+    main()
---
+++ web_crawler.py
@@ -0,0 +1,74 @@
+''' Extracting Video Title from a youtube playlists '''
+import time  # to calculate the execution time
+
+t1 = time.time()
+
+
+import requests
+
+
+def writeSourceContentToTextFile(response_content_list):
+    # writing the source content to a text file
+    with open('page_source.txt', 'w') as file:
+        for i in response_content_list:
+            file.write(i + '\n')
+        file.close()
+
+
+def extractTilesFromSourceContent():
+    titles = []
+    with open('page_source.txt', 'r') as file_r:
+        for line in file_r.readlines():
+            f_index = line.find("data-title=")  # > 3000
+            if f_index != -1:
+                start_index = f_index + 12
+                req_str = ''
+                while line[start_index] != '"':
+                    req_str = req_str + line[start_index]
+                    start_index += 1
+
+                titles.append(req_str)
+    return titles
+
+
+def getSourceContent(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    response_content = response.content
+    response_content_list = str(response_content).split('\\n')
+    # print('Number of lines in the SourceFile: ', len(response_content_list))
+
+    return response_content_list
+
+
+
+
+''' returns list of titles of the passed url '''
+def extract(url):
+
+    # gets the source content of the passed url in string format
+    response_content_list = getSourceContent(url)
+
+    # -----------------------------
+    # writes the source content to a text file
+    writeSourceContentToTextFile(response_content_list)
+
+    # -----------------------------
+    ''' Extracting the titles '''
+    titles = extractTilesFromSourceContent()
+
+    print('Number of videos: ', len(titles))
+
+    # writing the extracted titles to a text file
+    with open('extracted_titles.txt', 'w') as file_w:
+        file_w.write('url= ' + url + '\n\n')
+        for count, value in enumerate(titles):
+            file_w.write(str(count + 1) + "  " + value)
+
+            file_w.write('\n')
+
+    return titles
+
+    # print("\nCheck 'source_1_titles.txt' file for list the of titles!")
+    # print('It took {} sec(s) to complete the task!'.format(round(time.time() - t1)))
